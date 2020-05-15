27643b95dcb6f2973ce7bf0396e14d5eccd6f1d9
rusty-mark <markpacker81@gmail.com>
2020-05-15 13:37:15 +0100

    First Commit

---
+++ Mavenjava/src/test/java/hero/Appium_159_Test.java
@@ -0,0 +1,18 @@
+package hero;
+
+import org.testng.annotations.Test;
+
+public class Appium_159_Test {
+	
+	@Test
+	public void Appium_159_Test_1() {
+		System.out.println("Appium_159_Test_1");
+	}
+	
+	@Test
+	public void Appium_159_Test_2() {
+		System.out.println("Appium_159_Test_2");
+	}
+
+
+}
---
+++ Mavenjava/src/test/java/hero/RestAPI_159_Test.java
@@ -0,0 +1,16 @@
+package hero;
+
+import org.testng.annotations.Test;
+
+public class RestAPI_159_Test {
+	
+	@Test
+	public void RestAPI_159_Test_1() {
+		System.out.println("RestAPI_159_Test_1");
+	}
+	
+	@Test
+	public void TRestAPI_159_Test_2() {
+		System.out.println("RestAPI_159_Test_2");
+	}
+}
